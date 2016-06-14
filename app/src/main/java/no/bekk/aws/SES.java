725fda9a4ba3af589793b352eef7ea37303f19b8
Henrik Wingerei <henrik.wingerei@bekk.no>
2016-06-14 22:44:25 +0200

    Løsningsforslag for SES (i SES.java)

--- app/src/main/java/no/bekk/aws/SES.java
+++ app/src/main/java/no/bekk/aws/SES.java
@@ -3,13 +3,22 @@ package no.bekk.aws;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
+import com.amazonaws.services.simpleemail.model.*;
 
 import java.io.IOException;
 
+
 public class SES {
 
-    /*public static void main(String[] args) throws IOException {
+    static final String FROM = "test@example.com";  // Epost-adressen må verifiseres i SES-console
+    static final String TO = "test@example.com"; // Epost-adressen må verifiseres i SES-console
+    static final String BODY = "This email was sent through Amazon SES by using the AWS SDK for Java.";
+    static final String SUBJECT = "Amazon SES test (AWS SDK for Java)";
+
+    public static void main(String[] args) throws IOException {
 
         AWSCredentials credentials;
         try {
@@ -23,6 +32,20 @@ public class SES {
         }
 
         AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient(credentials);
+        Region region = Region.getRegion(Regions.EU_WEST_1);
+        client.setRegion(region);
+
+        Destination destination = new Destination().withToAddresses(TO);
+
+        Content subject = new Content().withData(SUBJECT);
+        Content textBody = new Content().withData(BODY);
+        Body body = new Body().withText(textBody);
+
+        Message message = new Message().withSubject(subject).withBody(body);
+
+        SendEmailRequest request = new SendEmailRequest().withSource(FROM).withDestination(destination).withMessage(message);
+
+        client.sendEmail(request);
+        System.out.println("Email sent!");
     }
-    */
 }
